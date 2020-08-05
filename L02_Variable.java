50c833df6794854344ac7b0eacf25cba10a4114b
Boo Sung Jun <55964775+boorooksus@users.noreply.github.com>
2020-08-06 00:14:10 +0900

    Add files via upload

---
+++ L02_Variable.java
@@ -0,0 +1,21 @@
+
+public class L02_Variable {
+	
+	final static double PI = 3.14;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int intType = 100;
+		double doubleType = 150.5;
+		String stringType = "hi";
+		
+		System.out.println(intType);
+		System.out.println(doubleType);
+		System.out.println(stringType);
+		
+		System.out.println(PI);
+		
+		System.out.println(intType + PI);
+	}
+
+}
