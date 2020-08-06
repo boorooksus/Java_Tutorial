e01c5824fabe7411d60b0fade769c464eeb3e2d8
Boo Sung Jun <55964775+boorooksus@users.noreply.github.com>
2020-08-06 19:10:21 +0900

    Add files via upload

---
+++ L03_DataType.java
@@ -0,0 +1,17 @@
+
+public class L03_DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(char i = 'a'; i <= 'z'; i++) {
+			System.out.print(i +  " ");
+		}
+		
+		System.out.format("%n8Áø¼ö: %x%n", 16);
+		
+		String name = "Hello world!";
+		System.out.println(name);
+		System.out.println(name.substring(0,5));
+	}
+
+}
