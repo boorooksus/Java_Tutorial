e3a198c1a9bbcc23a49ca99d4576fc0d7dd7c6f2
Boo Sung Jun <55964775+boorooksus@users.noreply.github.com>
2020-08-06 19:39:28 +0900

    Add files via upload

---
+++ L05_ConditionalStatement.java
@@ -0,0 +1,16 @@
+
+public class L05_ConditionalStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Hello world!";
+		
+		if (str.contains("Hi")){
+			System.out.println("True");
+		}
+		else {
+			System.out.println("False");
+		}
+	}
+
+}
